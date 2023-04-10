43ea32b2c5b77ff7eb11caf97532e2180dc33f6b
aniket <aniketchavan505@gmail.com>
2023-04-10 14:51:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+
+const port = 3000;
+
+
+app.use(cors())
+
+app.use(express.json())
+
+
+app.get('/',(req,res)=>{
+	console.log('get Request')
+})
+
+
+app.post('/',(req,res)=>{
+	console.log('Hello World')
+	console.log(req.params,"params")
+	console.log(req.query,"query")
+	console.log(res.body, "body")  // Undefine
+	console.log(res, "all") // giving all the data
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
